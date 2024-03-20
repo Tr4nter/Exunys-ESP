3b36a38c9171ed2d4bd193acd96b30da5d23c92d
Tr4nt <126683818+Tr4nter@users.noreply.github.com>
2024-03-20 11:52:23 +0700

    Update ESP.lua

--- src/ESP.lua
+++ src/ESP.lua
@@ -1,10 +1,10 @@
 local game = game
 local select, pcall, loadstring , warn = select, pcall, loadstring, warn
 
-local Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/Original.lua"))))
+local Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Tr4nter/Exunys-ESP/main/src/Modules/Original.lua"))))
 
 if not Success then
-    Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/UWP%20Support.lua"))))
+    Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Tr4nter/Exunys-ESP/main/src/Modules/UWP%20Support.lua"))))
 
     if not Success then
         return warn("EXUNYS_ESP > Loader - Your script execution software does not support this module.")
